881d7a23d8f52dabf3ab906a1c621e5c73b496b5
Edwardd730 <ed@prosvio.com>
2026-02-25 18:03:25 -0800

    updated port for railway

--- server.js
+++ server.js
@@ -86,4 +86,8 @@ async function processUrls(urls) {
   currentJob.running = false;
 }
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
